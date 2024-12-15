f4ce73688151d3cfd4001c211fce767c47f09630
Marek Swiderek <maronq1212@gmail.com>
2024-12-15 21:46:50 +0100

    Prepared Airport & Flight models, added association between them. Prepared seed data for airports and flights.

---
+++ db/migrate/20241215184624_create_airports.rb
@@ -0,0 +1,10 @@
+class CreateAirports < ActiveRecord::Migration[7.2]
+  def change
+    create_table :airports do |t|
+      t.string :airport_code, index: {unique: true}, null: false
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20241215185452_create_flights.rb
@@ -0,0 +1,13 @@
+class CreateFlights < ActiveRecord::Migration[7.2]
+  def change
+    create_table :flights do |t|
+      t.references :departure_airport, index: true, null: false, foreign_key: {to_table: :airports}
+      t.references :arrival_airport, index: true, null: false, foreign_key: {to_table: :airports}
+      t.datetime :departure_time, null: false
+      t.time :duration, null: false
+      t.decimal :price, precision: 8, scale: 2, null: false
+
+      t.timestamps
+    end
+  end
+end
