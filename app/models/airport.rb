51734f4e4086d1e68781aba93a3e065efa822005
Marek Swiderek <maronq1212@gmail.com>
2024-12-16 13:14:05 +0100

    Generated flights controller with index action and flights#index view prototype

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,7 @@
 class Airport < ApplicationRecord
   has_many :departure_flights, foreign_key: "departure_airport_id", class_name: "Flight"
   has_many :arrival_flights, foreign_key: "arrival_airport_id", class_name: "Flight"
+
+  validates :airport_code, presence: true, uniqueness: true, length: {is: 3}
+  validates :name, presence: true, length: {minimum: 5, maximum: 30}
 end
