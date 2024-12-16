567e04afb57520be4875a9061555b2d03f74471f
Marek Swiderek <maronq1212@gmail.com>
2024-12-16 16:34:34 +0100

    Added validations to airport & flight models. Created flights#find_flights action and prepared views, partial views for search and results page(single page of flights#index).

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,26 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @airports_options = Airport.all.map { |a | [ a.airport_code, a.id ] }
+    @flight = Flight.first
+
+    # checking presence of params[:departure_time] because it is only field that has no value by default
+    if params[:departure_time].present?
+      @flights = find_flights(params)
+    else
+      @flights = find_flights(nil)
+    end
+  end
+
+  def show
+  end
+
+  def find_flights(find_flights_params)
+    if find_flights_params.nil?
+      @flights = Flight.none
+    else
+      # TO-DO: add departure_time and number of passengers parameters handling
+      @flights = Flight.where(departure_airport_id: find_flights_params[:departure_airport_id],
+                              arrival_airport_id: find_flights_params[:arrival_airport_id])
+    end
   end
 end
