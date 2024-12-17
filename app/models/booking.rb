0af77357f74b50dfbe6e6756d00d5ee2dcf8c188
Marek Swiderek <maronq1212@gmail.com>
2024-12-17 15:56:00 +0100

    Added presence verification for email field in Passenger model, added validations for Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :flight
   belongs_to :passenger
+
+  validates :flight_id, :passenger_id, presence: true
+  validates :payment_verified, inclusion: { in: [true, false] }, presence: true
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -6,5 +6,5 @@ class Passenger < ApplicationRecord
 
   validates :passport_no, uniqueness: { case_sensitive: false }, length: { is: 9 }
   validates :name, :surname, length: { minimum: 2, maximum: 30 }
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
 end
