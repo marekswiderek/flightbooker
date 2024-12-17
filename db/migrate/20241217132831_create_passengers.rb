32e10f07c00730ae75c4142ccec5ec9253cae0f2
Marek Swiderek <maronq1212@gmail.com>
2024-12-17 15:29:43 +0100

    Fix lint errors

--- db/migrate/20241217132831_create_passengers.rb
+++ db/migrate/20241217132831_create_passengers.rb
@@ -11,18 +11,17 @@ class CreatePassengers < ActiveRecord::Migration[7.2]
 
     reversible do |direction|
       direction.up do
-        #execute SQL DDL query to SET PRIMARY KEY to avoid creating a sequence on passport_no column
+        # execute SQL DDL query to SET PRIMARY KEY to avoid creating a sequence on passport_no column
         execute <<-SQL
           ALTER TABLE passengers ADD PRIMARY KEY (passport_no);
         SQL
       end
       direction.down do
-        #revert UP action
+        # revert UP action
         execute <<-SQL
           ALTER TABLE passengers DROP CONSTRAINT passengers_pkey;
         SQL
       end
     end
-
   end
 end
