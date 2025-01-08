c75a44b18d86d0424d40cb58950eb97558f7627f
Marek Swiderek <maronq1212@gmail.com>
2025-01-08 16:45:17 +0100

    FIXED ISSUE WITH BOOLEAN payment_verified VALUE OF booking MODEL - default value of payment_verified is now set to false using before_create AR callback. Some styling for bookings#show view - added button for payment sandbox purposes - still not a real payment module integration

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,13 +1,13 @@
 class PaymentsController < ApplicationController
   def show
-    @booking = Booking.first
+    @booking = Booking.find(params[:id])
   end
 
   # Simulation for payment process
   # Bookings with payment_verified == false are deleted after 10 minutes
   def approve_payment
     @booking = Booking.find(params[:id])
-      @booking.payment_verified = true
+    @booking.payment_verified = true
       if @booking.save
         redirect_to @booking
       else
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  after_initialize :set_defaults
+  before_create :set_defaults
 
   belongs_to :flight
   has_many :passengers, inverse_of: :booking, dependent: :destroy
@@ -7,7 +7,7 @@ class Booking < ApplicationRecord
   accepts_nested_attributes_for :passengers
 
   validates :flight_id, :booking_value, presence: true
-  validates :payment_verified, inclusion: { in: [ true, false ] }
+  # validates :payment_verified, inclusion: { in: [ true, false ] }
 
   private
   def set_defaults
