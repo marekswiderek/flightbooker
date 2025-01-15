856d5db06b7d2f4124d39e0edc80a684d543c346
Marek Swiderek <maronq1212@gmail.com>
2025-01-15 14:30:42 +0100

    Added API endpoit for airports#index with JSON request format - /airports.json returning (name, airport_code) pairs of available airports

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -3,6 +3,11 @@ class AirportsController < ApplicationController
 
   def index
     @airports = Airport.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @airports }
+    end
   end
 
   def show
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -4,4 +4,12 @@ class Airport < ApplicationRecord
 
   validates :airport_code, presence: true, uniqueness: true, length: { is: 3 }
   validates :name, presence: true, length: { minimum: 5, maximum: 30 }
+
+  # def as_json(options = {})
+  #   super({ only: [ :name, :airport_code ] }).merge(options)
+  # end
+
+  def as_json(_options = {})
+    { name: self.name, airport_code: self.airport_code }
+  end
 end
